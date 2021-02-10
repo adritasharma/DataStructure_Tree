460761cb2e2d759d275a9bfd46240771ad5cb1aa
adritasharma <adritasharma.developer@gmail.com>
2021-02-10 15:33:48 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,8 @@ export class AppComponent {
   name = "Binary Tree";
   myBinaryTree: BinaryTreeNode;
 
+  preOrderData = [];
+
   constructor() {
     this.myBinaryTree = new BinaryTreeNode({ text: "root", value: 1 });
 
@@ -26,10 +28,25 @@ export class AppComponent {
     };
     let right = new BinaryTreeNode({ text: "Sub-2", value: 3 });
 
-    left.left = new BinaryTreeNode({ text: "Sub-1-1", value: 1 });
+    left.left = new BinaryTreeNode({ text: "Sub-1-1", value: 4 });
     left.right = new BinaryTreeNode({ text: "Sub-1-2", value: 5 });
 
     this.myBinaryTree.left = left;
     this.myBinaryTree.right = right;
+
+    this.printPreorder(this.myBinaryTree);
+  }
+
+  printPreorder(node: BinaryTreeNode) {
+    if (node == null) return;
+
+    /* first print data of node */
+    console.log(node.data.value + " ");
+    this.preOrderData.push(node.data.value);
+    /* then recur on left sutree */
+    this.printPreorder(node.left);
+
+    /* now recur on right subtree */
+    this.printPreorder(node.right);
   }
 }
