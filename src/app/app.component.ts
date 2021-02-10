0b093d5c0eb7298bfa046d23d489a6759069612b
adritasharma <adritasharma.developer@gmail.com>
2021-02-10 15:24:16 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,13 +19,16 @@ export class AppComponent {
   myBinaryTree: BinaryTreeNode;
 
   constructor() {
-    this.myBinaryTree = new BinaryTreeNode("root");
+    this.myBinaryTree = new BinaryTreeNode({ text: "root", value: 1 });
 
-    let left = {
-      data: 1
+    let left: BinaryTreeNode = {
+      data: { text: "Sub-1", value: 2 }
     };
-    let right = new BinaryTreeNode(2);
-    
+    let right = new BinaryTreeNode({ text: "Sub-2", value: 3 });
+
+    left.left = new BinaryTreeNode({ text: "Sub-1-1", value: 1 });
+    left.right = new BinaryTreeNode({ text: "Sub-1-2", value: 5 });
+
     this.myBinaryTree.left = left;
     this.myBinaryTree.right = right;
   }
